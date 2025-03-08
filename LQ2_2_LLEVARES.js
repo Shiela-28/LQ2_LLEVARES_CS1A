b4fe3e66dd308d79261223f97f215c953a8a5227
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-08 16:50:34 +0800

    Add files via upload

---
+++ LQ2_2_LLEVARES.js
@@ -0,0 +1,8 @@
+//Using a LOOP STATEMENT and promp()
+
+let dogBreed = prompt("Enter dog breed:");
+let count = prompt("How many times to print?");
+
+for (let i = 0; i < count; i++) {
+  console.log(dogBreed);
+}
