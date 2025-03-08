c37eff4eadf7309c082e0264d9a37b36c73a979f
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-08 16:49:33 +0800

    Delete LQ2_2_LLEVARES.js

--- LQ2_2_LLEVARES.js
+++
@@ -1,8 +0,0 @@
-//Using a LOOP STATEMENT and promp()
-
-let dogBreed = prompt("Enter dog breed:");
-let count = prompt("How many times to print?");
-
-for (let i = 0; i < count; i++) {
-  console.log(dogBreed);
-}
